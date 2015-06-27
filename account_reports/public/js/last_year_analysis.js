855b9b45980b6bf4935cfed84226b98f61743cea
indictrans <tejal.s@indictranstech.com>
2015-06-27 18:28:45 +0530

    [Report Replication-Trial Balance C,General Ledger Summary, Last Year Analyasis, Budget Analysis]

---
+++ account_reports/public/js/last_year_analysis.js
@@ -0,0 +1,68 @@
+frappe.provide("account_reports.last_year_analysis");
+
+account_reports.last_year_analysis = {
+	"filters": [
+		{
+			"fieldname":"company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("company"),
+			"reqd": 1
+		},
+		{
+			"fieldname":"fiscal_year",
+			"label": __("Fiscal Year"),
+			"fieldtype": "Link",
+			"options": "Fiscal Year",
+			"default": frappe.defaults.get_user_default("fiscal_year"),
+			"reqd": 1
+		},
+		
+		{
+			"fieldname":"month",
+			"label": __("Month"),
+			"fieldtype": "Select",
+			"options": "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec",
+			"default": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", 
+				"Dec"][frappe.datetime.str_to_obj(frappe.datetime.get_today()).getMonth()],
+		}
+	],
+	"formatter": function(row, cell, value, columnDef, dataContext, default_formatter) {
+		//console.log(default_formatter)
+		if (columnDef.df.fieldname=="account") {
+			value = dataContext.account_name;
+
+			columnDef.df.link_onclick = "account_reports.last_year_analysis.open_general_ledger(" + JSON.stringify(dataContext) + ")";
+			columnDef.df.is_tree = true;
+		}
+
+		value = default_formatter(row, cell, value, columnDef, dataContext);
+
+		if (!dataContext.parent_account) {
+			var $value = $(value).css("font-weight", "bold");
+			if (dataContext.warn_if_negative && dataContext[columnDef.df.fieldname] < 0) {
+				$value.addClass("text-danger");
+			}
+
+			value = $value.wrap("<p></p>").parent().html();
+		}
+
+		return value;
+	},
+	"open_general_ledger": function(data) {
+		if (!data.account) return;
+
+		frappe.route_options = {
+			"account": data.account,
+			"company": frappe.query_report.filters_by_name.company.get_value(),
+			"from_date": data.from_date,
+			"to_date": data.to_date
+		};
+		frappe.set_route("query-report", "General Ledger");
+	},
+	"tree": true,
+	"name_field": "account",
+	"parent_field": "parent_account",
+	"initial_depth": 3
+};
