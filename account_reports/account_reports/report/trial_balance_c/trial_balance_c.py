247ae88f10ec762047cc79afa6ac511b428029b7
indictrans <tejal.s@indictranstech.com>
2015-06-30 11:39:55 +0530

    [trial balance / budget analysis html file changes]

--- account_reports/account_reports/report/trial_balance_c/trial_balance_c.py
+++ account_reports/account_reports/report/trial_balance_c/trial_balance_c.py
@@ -9,7 +9,7 @@ from erpnext.accounts.report.financial_statements import filter_accounts, get_gl
 from frappe.utils import add_days, cint, cstr, date_diff, rounded, flt, getdate, nowdate, \
 	get_first_day, get_last_day,money_in_words, now, nowtime
 
-from account_reports.account_reports.utils import get_fiscal_year
+#from account_reports.account_reports.utils import get_fiscal_year
 
 value_fields = ("opening_debit", "opening_credit", "debit", "credit", "closing_debit", "closing_credit","ytd_debit","ytd_credit")
 
